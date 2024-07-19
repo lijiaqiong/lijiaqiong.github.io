c1e9b2061cf1e9022abf49adfba3d46631430295
zcjiaqiongli <jiaqiong.li@bangdao-tech.com>
2024-07-19 17:41:10 +0800

    Updates

---
+++ assets/docs_ai_ai.md.8KkrXBS6.js
@@ -0,0 +1,61 @@
+import{_ as s,c as a,o as n,a3 as i}from"./chunks/framework.D5qxx6u8.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"docs/ai/ai.md","filePath":"docs/ai/ai.md"}'),p={name:"docs/ai/ai.md"},l=i(`<blockquote><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">每日快讯 - 7.18</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：小米集团投资具身智能科技公司“小雨智造”。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 小米集团联合机器人领域专家王田苗和北京智源研究院，投资了一家新兴的具身智能科技公司“小雨智造”，该公司专注于开发“一脑多形”的智能机器人，并已在工业领域取得初步商业化进展。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://36kr.com/p/2866102868085639</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：OpenAI发布“超级对齐”团队的最终论文：提升AI生成内容的可读性。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: OpenAI通过“证明者-验证者”博弈机制，成功提升了AI模型生成内容的可读性，同时保持了高准确率，这项研究可能是该团队的“最后一篇”超级对齐论文。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.qbitai.com/2024/07/168179.html</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：通义千问、GPT-4o 等七款 AI 大模型“高考成绩”公布 ：前三名文科过一本，理科过二本。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：测试结果显示，书生・浦语 2.0 系列文曲星大模型（浦语文曲星）、阿里通义千问大模型 Qwen2-72B 以及 GPT-4o 再次包揽文、理科前三甲；前三名 AI“考生”的文、理科成绩分别超过了“一本”“二本”线（以今年高考人数最多的河南省的分数线为参考）。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/782/645.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：DeepL发布新一代语言模型：翻译质量超越GPT-4、谷歌和微软。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：DeepL最新推出的语言模型在翻译质量上取得重大突破，通过专有技术、数据和人工辅导，其翻译效果显著优于谷歌、微软和GPT-4。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://tech.ifeng.com/c/8bJaai4LLnI</span></span></code></pre></div></blockquote><blockquote><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">每日快讯 - 7.17</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：研究：生成式 AI 更像是记忆大师而非推理高手。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 麻省理工学院计算机科学与人工智能实验室（CSAIL）的一项最新研究指出，这些强大的大型语言模型（LLM）似乎更依赖记忆而不是真正的推理能力。它们在熟悉的任务上表现出色，但在面对新问题时却显得力不从心。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/782/419.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：特朗普盟友推动AI军事技术发展。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 特朗普的盟友正在起草一项AI行政命令，旨在启动类似“曼哈顿项目”的计划，以加速军事AI技术的发展，同时审查并简化现有法规，与拜登政府的AI政策形成鲜明对比。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://zhidx.com/p/434111.html</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：科技巨头被曝未经授权用 YouTube 内容训练 AI，苹果、英伟达在列。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：调查记者披露，一些世界上最富有的科技公司一直在利用来自成千上万个 YouTube 视频的素材来训练 AI，而这违反了 YouTube 禁止从平台上未经许可抓取内容的规定。据悉，超过 17.3 万个来自 4.8 万个频道的 YouTube 视频字幕文件被用来训练人工智能模型，其中就包括苹果、英伟达、Salesforce 等硅谷巨头。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/782/233.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：Anthropic发布安卓版Claude应用，拓展AI助手功能。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：Anthropic公司在iOS版本后，推出了适用于安卓8.0及以上版本的Claude应用，提供即时解答、深度工作协助和智能知识获取，旨在成为用户信赖的AI助手。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.sohu.com/a/793990562_104421</span></span></code></pre></div></blockquote><blockquote><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">每日快讯 - 7.16</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：腾讯推出3D角色生成应用，一键打造个性化3D形象。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 腾讯最新推出的“3D角色梦工厂”应用，通过一张正面头像照片，用户即可快速生成专属3D角色，支持全方位查看、分享、打印等多种功能，引领3D打印市场新趋势 DeepMind被指控抄袭开源研究，尽管在论文中引用了原告的工作，但原告认为DeepMind对差异性的描述荒谬，引发社区对学术诚信和评审流程的广泛讨论。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.qbitai.com/2024/07/166982.html</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：微软为 Excel 等研发新 AI 模型：性能较常规方案提高 25.6%，词元使用成本降低 96%。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 根据微软最新公布的研究论文，计划为 Excel、谷歌 Sheets 等电子表格应用程序，开发全新的 AI 大语言模型--SpreadsheetLLM。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/782/030.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：谷歌 Gemini AI 遭质疑：未经用户同意读取 Google Drive 文档。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：据 X 用户 @Kevin Bankston 的报告，谷歌的人工智能服务 Gemini AI 似乎在未经用户明确许可的情况下读取 Google Drive 中的私人文档。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/781/921.htm</span></span></code></pre></div></blockquote><blockquote><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">每日快讯 - 7.15</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：谷歌DeepMind涉嫌抄袭事件。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: DeepMind被指控抄袭开源研究，尽管在论文中引用了原告的工作，但原告认为DeepMind对差异性的描述荒谬，引发社区对学术诚信和评审流程的广泛讨论。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.qbitai.com/2024/07/166525.html</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：中国农业大学发布“神农大模型 2.0”：覆盖育种、种植、养殖、农业遥感及气象。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: 相比 1.0 版本，“神农大模型 2.0”在图像、声音、视频、文件等多模态交互及智能化推理方面获得提升，使大模型能够覆盖育种、种植、养殖、农业遥感及气象等多个农业应用场景。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/781/780.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：百度推出仿真人 AI 社交 App“文小言”：可与虚拟角色互动并建立情感连接。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：百度近期上线了一款名为“文小言”的 AI 数字人社交 App，其基于文心大模型技术打造，是一款与 AI 虚拟角色进行实时沟通、互动并建立情感连接的人工智能应用服务。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.ithome.com/0/781/940.htm</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">🚀推荐阅读：豆包文生图功能升级，网页端新功能上线。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">📌快速提炼: ：豆包宣布其网页端文生图功能经过优化，新增参考图生成、比例调整及风格定制等功能，目前处于内测阶段，用户可申请体验。</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">来源：https://www.pingwest.com/w/296653</span></span></code></pre></div></blockquote>`,4),e=[l];function t(h,E,k,c,d,o){return n(),a("div",null,e)}const m=s(p,[["render",t]]);export{g as __pageData,m as default};
---
+++ assets/docs_learning_2024.2.29.md.leGEfvYV.js
@@ -0,0 +1,40 @@
+import{_ as s,c as n,o as a,a3 as i}from"./chunks/framework.D5qxx6u8.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"docs/learning/2024.2.29.md","filePath":"docs/learning/2024.2.29.md"}'),p={name:"docs/learning/2024.2.29.md"},l=i(`<div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">最近技术学报已送达☀️☀️</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【近期资讯】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 教育部公布184个中小学人工智能教育基地名单，推动AI教育发展</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 微软开源自动化框架PyRIT，帮助专家和工程师识别生成式AI模型风险</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">3.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 华为发布5G-A全系列产品与解决方案，发布通信行业首个大模型</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">4.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 南京大学面向全体本科新生开设“人工智能通识核心课程体系”，为全国高校首例</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【行业动态】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">谷歌DeepMind推出AI基础世界模型Genie，可基于图片和提示词生成2D游戏</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【文章推荐】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">职场焦虑症｜如何做自己的思想工作？</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">https://mp.weixin.qq.com/s/G3wpFnJoOJpY4XKhF0aGqQ</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">人一旦开窍所有事都有规律</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">https://mp.weixin.qq.com/s/aKOBIwwRd3kZiz4aL9Azzg</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">AI科普在飞书：一众知识付费内容的免费源头找到了！ </span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">https://mp.weixin.qq.com/s/3DVgBCMzrqZUnkk8ASfi2Q</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">如何有效克服惰性 </span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">https://mp.weixin.qq.com/s/VldgHaPzo3KkKcdzRrou1w</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【今日一言】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">任何学科学习的本质都是复盘复盘再复盘。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【工具使用】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">使用新版Intellij IDEA ，拥抱工具新变化</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">【问题思考】</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 很有意思的现象：企业和个人都全面在拥抱AI，但是在内容平台却在加大AI内容的识别，提示，一边是拥抱AI，另一边是抗拒AI</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">2·了解到一个做事原则：尽可能少做事，以最低的工作量来达成工作目标，</span></span></code></pre></div>`,1),e=[l];function t(h,E,k,c,d,r){return a(),n("div",null,e)}const y=s(p,[["render",t]]);export{o as __pageData,y as default};
